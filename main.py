695f2d5e6f8bad328c9deedc3d3338b345e59027
grostaco <grostaco@gmail.com>
2021-03-17 03:05:23 +0700

    Made a mock classifier

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-import json
+from impression.message import DiscordMetaChannel
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import GridSearchCV
+from sklearn import metrics
+from operator import itemgetter
 
-def parse_messages(path, content_type="discord") :
-    content = json.load(path)
+dmc = DiscordMetaChannel('info.json')
+text_clf = Pipeline([
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+    ('clf', MultinomialNB()),
+])
+
+dataset = [(x["content"], x["author"]["name"]) for x in dmc if x["type"] == "Default" and
+           x["author"]["name"] in {"~Chansey~", "(traits*)sbrk(sizeof *traits)", "RoaR",
+                                   "🍔🥤MrChacocha🥤🍔"}]
+
+X_train, y_train, X_test, y_test = tuple(map(itemgetter(0), dataset[:50000])), tuple(map(itemgetter(1), dataset[:50000])),\
+                                   tuple(map(itemgetter(0), dataset[50000:])), tuple(map(itemgetter(1), dataset[50000:]))
+
+text_clf.fit(X_train, y_train)
+predicted = text_clf.predict(X_test)
+
+while True :
+    x = input("Text to predict: ")
+    print(text_clf.predict([x])[0])
