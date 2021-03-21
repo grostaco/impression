2200ce00eff7a6f7db52c0d325956a820386301a
grostaco <grostaco@gmail.com>
2021-03-21 09:12:36 +0700

    Added export_model

--- impression/model.py
+++ impression/model.py
@@ -2,75 +2,69 @@ import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras import losses
 from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
-from impression.message import DiscordChannelContext
 
 
-def vectorize_text(text, label) :
-    text = tf.expand_dims(text, -1)
-    return vectorize_layer(text), label
+def export_model(path, train_path='train', test_path='test',
+                 validation_split=0.2, seed=42,
+                 batch_size=32,
+                 max_features=5000, embedding_dim=128, sequence_length=500) :
 
+    raw_train_ds = tf.keras.preprocessing.text_dataset_from_directory(
+        train_path, batch_size=batch_size, validation_split=validation_split, subset='training', seed=seed)
 
-dcc = DiscordChannelContext("info.json")
-dcc.export('train', end=-5000, whitelist={'394083584878837762', '319362246205767681', '448122057054617603', '239661359963439124'})
-dcc.export('test', start=-5000,whitelist={'394083584878837762', '319362246205767681', '448122057054617603', '239661359963439124'})
+    raw_val_ds = tf.keras.preprocessing.text_dataset_from_directory(
+        train_path, batch_size=batch_size, validation_split=validation_split, subset='validation', seed=seed)
 
-batch_size = 32
-raw_train_ds = tf.keras.preprocessing.text_dataset_from_directory(
-    'train', batch_size=batch_size, validation_split=0.2, subset='training', seed=42)
+    raw_test_ds = tf.keras.preprocessing.text_dataset_from_directory(test_path, batch_size=batch_size)
 
-raw_val_ds = tf.keras.preprocessing.text_dataset_from_directory(
-    'train', batch_size=batch_size, validation_split=0.2, subset='validation', seed=42)
+    vectorize_layer = TextVectorization(
+        max_tokens=max_features,
+        output_mode='int',
+        output_sequence_length=sequence_length
+    )
 
-raw_test_ds = tf.keras.preprocessing.text_dataset_from_directory('test', batch_size=batch_size)
+    def vectorize_text(text, label):
+        text = tf.expand_dims(text, -1)
+        return vectorize_layer(text), label
 
-max_features = 5000
-embedding_dim = 128
-sequence_length = 500
+    text_ds = raw_train_ds.map(lambda x, y: x)
+    vectorize_layer.adapt(text_ds)
 
-vectorize_layer = TextVectorization(
-    max_tokens=max_features,
-    output_mode='int',
-    output_sequence_length=sequence_length
-)
+    train_ds = raw_train_ds.map(vectorize_text)
+    val_ds = raw_val_ds.map(vectorize_text)
+    test_ds = raw_test_ds.map(vectorize_text)
 
-text_ds = raw_train_ds.map(lambda x, y: x)
-vectorize_layer.adapt(text_ds)
+    AUTOTUNE = tf.data.experimental.AUTOTUNE
 
-train_ds = raw_train_ds.map(vectorize_text)
-val_ds = raw_val_ds.map(vectorize_text)
-test_ds = raw_test_ds.map(vectorize_text)
+    train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-AUTOTUNE = tf.data.experimental.AUTOTUNE
+    model = tf.keras.Sequential([
+        layers.Embedding(max_features + 1, embedding_dim),
+        layers.Dropout(0.2),
+        layers.GlobalAveragePooling1D(),
+        layers.Dropout(0.2),
+        layers.Dense(4)
+    ])
 
-train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
-test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    model.compile(
+        loss=losses.SparseCategoricalCrossentropy(from_logits=True),
+        optimizer='adam',
+        metrics=['accuracy']
+    )
 
-model = tf.keras.Sequential([
-    layers.Embedding(max_features + 1, embedding_dim),
-    layers.Dropout(0.2),
-    layers.GlobalAveragePooling1D(),
-    layers.Dropout(0.2),
-    layers.Dense(4)
-])
+    history = model.fit(
+        train_ds,
+        validation_data=val_ds,
+        epochs=10
+    )
 
-model.compile(
-    loss=losses.SparseCategoricalCrossentropy(from_logits=True),
-    optimizer='adam',
-    metrics=['accuracy']
-)
+    model.evaluate(test_ds)
 
-history = model.fit(
-    train_ds,
-    validation_data=val_ds,
-    epochs=10
-)
-
-model.evaluate(test_ds)
-
-export_model = tf.keras.Sequential([
-    vectorize_layer,
-    model,
-    layers.Activation('softmax')
-])
-export_model.save("pred")
+    model_export = tf.keras.Sequential([
+        vectorize_layer,
+        model,
+        layers.Activation('softmax')
+    ])
+    model_export.save(path)
